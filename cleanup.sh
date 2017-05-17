02a2f21f31b7bdf86a953804ed89dc3436724541
Rodrigo Valle <rdan.valle@gmail.com>
2017-05-17 15:55:58 -0700

    Cleanup clears Chrome data & lists open git repos

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+set -e
 
+# clear git credentials
 printf "host=github.com\nprotocol=https\n\n" | git credential-osxkeychain erase
-git config --unset-all user.name
-git config --unset-all user.email
+git config --unset-all user.name || true
+git config --unset-all user.email || true
 
-echo "remember to:"
-echo " - delete any open repositories"
-echo " - log out of chrome"
+# clear chrome data
+if [[ ! $(pgrep 'Google Chrome') ]]; then
+  rm -rf ~/Library/Application\ Support/Google/Chrome/Default
+else
+  echo 'please close chrome'
+  exit 1
+fi
+
+# list open git repos
+echo "remember to close chrome and delete any open repositories:"
+git_repos=$(find ~ -type d -name '.vim' -prune -o -name '.git' -print)
+
+for git_repo in $git_repos; do
+  fullpath=$(basename $(dirname $git_repo))
+  echo " ~/${fullpath#$HOME}"
+done
